be4b4bf76a2e77131f7a8ee39d1ba67c706d8732
sammydowds <sammydowds1993@gmail.com>
2020-06-20 22:43:46 -0500

    Setting up rest app in django project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/samueldowds/Desktop/Projects/Current/backend_automate_pm/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

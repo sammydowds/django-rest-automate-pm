d263b38ede9e8ba6d1d0b9d6976a64776b6f84e2
sammydowds <sammydowds1993@gmail.com>
2020-06-20 23:24:03 -0500

    Created initial models

--- djangorestautomatepm/restautomatepm/models.py
+++ djangorestautomatepm/restautomatepm/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Projects(models.Model): 
+    name = models.CharField()
+    description = models.TextField()
+    complete = models.BooleanField()
+    status = models.BooleanField()
+    lastupdated = models.DateField()
+    company = models.CharField() 
+
+class Phases(models.Model): 
+    name = models.CharField()
+    description = models.TextField()
+    start = models.DateField()
+    end = models.DateField() 
+    complete = models.BooleanField()
+    active = models.BooleanField()
+    lastupdated = models.DateField()
+    projectId: models.IntegerField()
+    
+class Log(models.Model):
+    description = models.TextField()
+    notes = models.TextField()
+    timestamp = models.DateField()
+    projectId = models.IntegerField() 
+
+# Think about adding meta data if necessary 
